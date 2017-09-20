25011620f5a156cc3bed77b68f73f26b0e52f73d
Revtov_AS <pau7.1994@gmail.com>
2017-09-20 22:49:53 +0300

    Add first test with selenium java

--- java-example/src/main/java/ru/stqa/selenium/example/MyFirstTest.java
+++
@@ -1,5 +0,0 @@
-package ru.stqa.selenium.example;
-
-public class MyFirstTest {
-
-}
---
+++ java-example/src/test/java/ru/stqa/selenium/example/MyFirstTest.java
@@ -0,0 +1,41 @@
+package ru.stqa.selenium.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "/home/alex/Загрузки/chromedriver");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://google.com/");
+        WebElement searchWebDriver = driver.findElement(By.name("q"));
+        searchWebDriver.sendKeys("webdriver");
+        searchWebDriver.sendKeys(Keys.ENTER);
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
