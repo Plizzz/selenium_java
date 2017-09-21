107b46e6f0393b1d47330da0fa19c14aa3b2e25b
Revtov_AS <pau7.1994@gmail.com>
2017-09-21 17:28:57 +0300

    Added script to login to the account Added to gitignore the folder java-example/out/

--- java-example/src/test/java/ru/stqa/selenium/example/login_script.java
+++ java-example/src/test/java/ru/stqa/selenium/example/login_script.java
@@ -1,4 +1,37 @@
 package ru.stqa.selenium.example;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class login_script {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "/home/alex/Загрузки/chromedriver");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void loginScript() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop() {
+        System.out.println("close Chrome");
+        driver.quit();
+        driver = null;
+    }
 }
