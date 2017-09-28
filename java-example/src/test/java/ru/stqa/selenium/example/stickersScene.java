9776c1c530e96ec85c19d0cb8de77f860c66aa8d
Revtov_AS <pau7.1994@gmail.com>
2017-09-28 23:04:18 +0300

    Done the task №8

--- java-example/src/test/java/ru/stqa/selenium/example/stickersScene.java
+++ java-example/src/test/java/ru/stqa/selenium/example/stickersScene.java
@@ -1,4 +1,31 @@
 package ru.stqa.selenium.example;
 
-public class stickersScene {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class stickersScene extends TestBase {
+
+    @Test
+    public void stickers() {
+        driver.get("http://localhost/litecart/");
+
+        WebElement products = driver.findElement(By.cssSelector(".middle > .content"));
+        List <WebElement> box = products.findElements(By.className("box"));
+
+        for (int i = 0; i < box.size(); i++) {
+            List <WebElement> ducks = box.get(i).findElements(By.tagName("li"));
+
+            System.out.println("\n"+box.get(i).findElement(By.className("title")).getText());
+
+            for (int j = 0; j < ducks.size(); j++) {
+                WebElement sticker = ducks.get(j).findElement(By.className("sticker"));
+                String duckName = ducks.get(j).findElement(By.className("name")).getText();
+
+                System.out.println(duckName + " is " + sticker.getText());
+            }
+        }
+    }
 }
