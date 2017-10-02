6746e30f5f2c0c52da6d5cef6e41e3d51066f29a
Revtov_AS <pau7.1994@gmail.com>
2017-10-03 00:06:24 +0300

    Done the task №10 (c,d,e) Check colors for a specific color and compare font sizes

--- java-example/src/test/java/ru/stqa/selenium/example/correctProduct.java
+++ java-example/src/test/java/ru/stqa/selenium/example/correctProduct.java
@@ -14,8 +14,19 @@ public class correctProduct extends TestBase {
         String nameOfProduct1 = campaignsBlock.findElement(By.className("name")).getText();
 
         WebElement prices = campaignsBlock.findElement(By.className("price-wrapper"));
-        String regularPrice1 = prices.findElement(By.className("regular-price")).getText();
-        String campaignPrice1 = prices.findElement(By.className("campaign-price")).getText();
+        WebElement regularPrice = prices.findElement(By.className("regular-price"));
+        WebElement campaignPrice = prices.findElement(By.className("campaign-price"));
+
+        String regularPrice1 = regularPrice.getText();
+        String campaignPrice1 = campaignPrice.getText();
+
+        String colorOfRegularPrice1 = regularPrice.getCssValue("color");
+        String textDecorOfRegularPrice1 = regularPrice.getCssValue("text-decoration-line");
+        String fontSizeOfRegularPrice1 = regularPrice.getCssValue("font-size");
+
+        String colorOfCampaignPrice1 = campaignPrice.getCssValue("color");
+        String textDecorOfCampaignPrice1 = campaignPrice.getCssValue("font-weight");
+        String fontSizeOfCampaignPrice1 = campaignPrice.getCssValue("font-size");
 
         campaignsBlock.findElement(By.className("link")).click();
 
@@ -23,11 +34,38 @@ public class correctProduct extends TestBase {
         String nameOfProduct2 = campaignsBlock.findElement(By.className("title")).getText();
 
         prices = campaignsBlock.findElement(By.className("price-wrapper"));
-        String regularPrice2 = prices.findElement(By.className("regular-price")).getText();
-        String campaignPrice2 = prices.findElement(By.className("campaign-price")).getText();
+        regularPrice = prices.findElement(By.className("regular-price"));
+        campaignPrice = prices.findElement(By.className("campaign-price"));
+
+        String regularPrice2 = regularPrice.getText();
+        String campaignPrice2 = campaignPrice.getText();
+
+        String colorOfRegularPrice2 = regularPrice.getCssValue("color");
+        String textDecorOfRegularPrice2 = regularPrice.getCssValue("text-decoration-line");
+        String fontSizeOfRegularPrice2 = regularPrice.getCssValue("font-size");
+
+        String colorOfCampaignPrice2 = campaignPrice.getCssValue("color");
+        String textDecorOfCampaignPrice2 = campaignPrice.getCssValue("font-weight");
+        String fontSizeOfCampaignPrice2 = campaignPrice.getCssValue("font-size");
 
         System.out.println(nameOfProduct1 + " = " + nameOfProduct2);
         System.out.println(regularPrice1 + " = " + regularPrice2);
         System.out.println(campaignPrice1 + " = " + campaignPrice2);
+        System.out.println("------------------------------------------------");
+        System.out.println("Color of regular price in the mainpage: " + colorOfRegularPrice1);
+        System.out.println("Text decoration of regular price in the mainpage: " + textDecorOfRegularPrice1);
+        System.out.println("Font size of regular price in the mainpage: " + fontSizeOfRegularPrice1);
+        System.out.println("------------------------------------------------");
+        System.out.println("Color of regular price in the productpage: " + colorOfRegularPrice2);
+        System.out.println("Text decoration of regular price in the productpage: " + textDecorOfRegularPrice2);
+        System.out.println("Font size of regular price in the productpage: " + fontSizeOfRegularPrice2);
+        System.out.println("------------------------------------------------");
+        System.out.println("Color of campaign price in the mainpage: " + colorOfCampaignPrice1);
+        System.out.println("Text decoration of regular price in the mainpage: " + textDecorOfCampaignPrice1);
+        System.out.println("Font size of regular price in the mainpage: " + fontSizeOfCampaignPrice1);
+        System.out.println("------------------------------------------------");
+        System.out.println("Color of campaign price in the productpage: " + colorOfCampaignPrice2);
+        System.out.println("Text decoration of regular price in the productpage: " + textDecorOfCampaignPrice2);
+        System.out.println("Font size of regular price in the productpage: " + fontSizeOfCampaignPrice2);
     }
 }
