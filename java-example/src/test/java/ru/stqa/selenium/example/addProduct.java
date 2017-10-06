7fa72096849fe2d520601024f245735a8d194d89
Revtov_AS <pau7.1994@gmail.com>
2017-10-06 18:21:48 +0300

    Intermediate solution

--- java-example/src/test/java/ru/stqa/selenium/example/addProduct.java
+++ java-example/src/test/java/ru/stqa/selenium/example/addProduct.java
@@ -25,7 +25,7 @@ public class addProduct extends TestBase {
         WebElement code = generalTab.findElement(By.cssSelector("tr:nth-child(3)"));
         code.findElement(By.tagName("input")).sendKeys(randomAlphaNumeric(5));
 
-        WebElement categories = generalTab.findElement(By.cssSelector("tr:nth-child(4)"));
+        //WebElement categories = generalTab.findElement(By.cssSelector("tr:nth-child(4)"));
 
         WebElement prodGroups = generalTab.findElement(By.cssSelector("tr:nth-child(7) table"));
         prodGroups.findElement(By.cssSelector("tr:nth-child(4) input[type=\"checkbox\"]")).click();
@@ -36,8 +36,8 @@ public class addProduct extends TestBase {
         quantity.sendKeys("100");
 
         WebElement soldOutStatus = quantityTable.findElement(By.cssSelector("td:last-child"));
-        soldOutStatus.findElement(By.cssSelector("select")).click();
-        soldOutStatus.findElement(By.cssSelector(" option[value=\"2\"]")).click();
+        soldOutStatus.findElement(By.tagName("select")).click();
+        soldOutStatus.findElement(By.cssSelector("option[value=\"2\"]")).click();
 
         WebElement image = generalTab.findElement(By.cssSelector("tr:nth-child(9)"));
         image.findElement(By.tagName("input")).sendKeys(new File("./LinuxToy.jpg").getAbsolutePath());
@@ -49,5 +49,36 @@ public class addProduct extends TestBase {
         dateTo.findElement(By.tagName("input")).sendKeys("06-10-2019");
 
         driver.findElement(By.cssSelector(".index li:nth-child(2) a")).click();
+
+        WebElement infoTab = driver.findElement(By.id("tab-information"));
+
+        WebElement manufacturer = infoTab.findElement(By.cssSelector("tr select"));
+        manufacturer.click();
+        manufacturer.findElement(By.cssSelector("option[value=\"1\"")).click();
+
+        WebElement keywords = infoTab.findElement(By.cssSelector("tr:nth-child(3) input"));
+        keywords.sendKeys("Linux");
+
+        WebElement shortDesc = infoTab.findElement(By.cssSelector("tr:nth-child(4) input"));
+        shortDesc.sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                "Suspendisse sollicitudin ante massa, eget ornare libero porta congue.");
+
+        WebElement desc = infoTab.findElement(By.cssSelector("tr:nth-child(5) .trumbowyg-editor"));
+        desc.sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                "Suspendisse sollicitudin ante massa, eget ornare libero porta congue. Cras scelerisque" +
+                "dui non consequat sollicitudin. Sed pretium tortor ac auctor molestie. Nulla facilisi." +
+                "Maecenas pulvinar nibh vitae lectus vehicula semper. Donec et aliquet velit. Curabitur non" +
+                "ullamcorper mauris. In hac habitasse platea dictumst. Phasellus ut pretium justo, sit amet" +
+                "bibendum urna. Maecenas sit amet arcu pulvinar, facilisis quam at, viverra nisi. Morbi sit" +
+                "amet adipiscing ante. Integer imperdiet volutpat ante, sed venenatis urna volutpat a. Proin" +
+                "justo massa, convallis vitae consectetur sit amet, facilisis id libero.");
+
+        WebElement headTitle = infoTab.findElement(By.cssSelector("tr:nth-child(6) input"));
+        headTitle.sendKeys("Pinguin");
+
+        WebElement metaDesc = infoTab.findElement(By.cssSelector("tr:nth-child(7) input"));
+        metaDesc.sendKeys("Pinguin");
+
+        driver.findElement(By.cssSelector(".index li:nth-child(4) a")).click();
     }
 }
