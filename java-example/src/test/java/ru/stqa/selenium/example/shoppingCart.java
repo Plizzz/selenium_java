05d6852510892bf3a801ff228067ce95e11bc4d2
Revtov_AS <pau7.1994@gmail.com>
2017-10-10 23:28:10 +0300

    Done the task №13

--- java-example/src/test/java/ru/stqa/selenium/example/shoppingCart.java
+++ java-example/src/test/java/ru/stqa/selenium/example/shoppingCart.java
@@ -1,25 +1,48 @@
 package ru.stqa.selenium.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 public class shoppingCart extends TestBase {
 
     @Test
     public void cart() {
         driver.get("http://localhost/litecart/en/");
+        for (int i = 1; i < 4; i++) {
+            WebElement content = driver.findElement(By.cssSelector("div.content ul.listing-wrapper"));
+            WebElement product = content.findElement(By.tagName("a"));
+            product.click();
 
-        WebElement content = driver.findElement(By.cssSelector("div.content ul.listing-wrapper"));
-        WebElement product = content.findElement(By.tagName("a"));
-        product.click();
+            WebElement quantity = driver.findElement(By.className("quantity"));
 
-        WebElement quantity = driver.findElement(By.className("quantity"));
-        System.out.println(quantity.getText());
-        driver.findElement(By.name("add_cart_product")).click();
-        wait.until(ExpectedConditions.stalenessOf(quantity));
-        quantity = driver.findElement(By.className("quantity"));
+            List<WebElement> size = driver.findElements(By.cssSelector("td.options select option"));
+            if (size.size() > 0) {
+                size.get(2).click();
+            }
+            driver.findElement(By.name("add_cart_product")).click();
+            wait.until(ExpectedConditions.textToBePresentInElement(quantity, "" + i));
 
-        driver.findElement(By.cssSelector("#breadcrumbs a")).click();
+            driver.findElement(By.cssSelector("#breadcrumbs a")).click();
+        }
+        WebElement cart = driver.findElement(By.id("cart"));
+        cart.findElement(By.className("link")).click();
+
+        for (int i = 1; i < 4; i++) {
+            if (isElementPresent(driver, By.cssSelector("#checkout-cart-wrapper em"))) {
+                break;
+            }
+            WebElement duckInCartForm = driver.findElement(By.cssSelector("form div a"));
+            List<WebElement> duckInTable = driver.findElements(By.cssSelector(".dataTable tr"));
+
+            Assert.assertTrue(duckInCartForm.getText().equals(duckInTable.get(1).findElement(By.className("item")).getText()));
+            driver.findElement(By.cssSelector("p:last-child button")).click();
+
+            wait.until(ExpectedConditions.stalenessOf(duckInTable.get(1)));
+        }
+
     }
 }
