f7bcf2fe5698815bdf287844e5d3107ba24568a0
Revtov_AS <pau7.1994@gmail.com>
2017-11-01 22:37:33 +0300

    Добавлен снятие скрина

--- java-example/src/test/java/ru/stqa/selenium/example/TestBase.java
+++ java-example/src/test/java/ru/stqa/selenium/example/TestBase.java
@@ -1,27 +1,53 @@
 package ru.stqa.selenium.example;
 
+import com.google.common.io.Files;
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Set;
 
 public class TestBase {
-    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
-    public WebDriver driver;
+    public static ThreadLocal<EventFiringWebDriver> tlDriver = new ThreadLocal<>();
+    public EventFiringWebDriver driver;
     public WebDriverWait wait;
 
+    public class myListener extends AbstractWebDriverEventListener {
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + " found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            super.onException(throwable, driver);
+            File tmp = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            File screen = new File("screen"+System.currentTimeMillis() + ".png");
+            try {
+                Files.copy(tmp, screen);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     @Before
     public void start() {
         if (tlDriver.get() != null) {
@@ -42,7 +68,8 @@ public class TestBase {
 //        catch (MalformedURLException e){
 //            e.printStackTrace();
 //        }
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new myListener());
         wait = new WebDriverWait(driver, 10);
         //driver = new FirefoxDriver();
 
